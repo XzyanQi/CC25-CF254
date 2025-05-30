a8c61223eb2a0ba752d14dcbe8b0e085d2854bde
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-05-30 14:49:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -85,5 +85,6 @@ def serve_static(path):
     return send_from_directory("static", path)
 
 if __name__ == "__main__":
-    print(" Flask siap jalan di http://127.0.0.1:5000/")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f" Flask siap jalan di http://0.0.0.0:{port}/")
+    app.run(host="0.0.0.0", port=port)
