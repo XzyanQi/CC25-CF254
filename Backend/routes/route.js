0fc5569fa533ecc46f1cca44602f1c7f4c7bc0c1
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-02 23:04:30 +0700

    Update route.js

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const router = express.Router();
-console.log("Memuat Backend/routes/route.js..."); 
 
-const auth = require("../apps/api/authApi"); 
 const user = require("../apps/api/userApi");
+const auth = require("../apps/api/authApi"); 
+
+
 const chatbotApi = require('../apps/api/chatbotApi.js'); 
 
-if (auth) {
-  console.log("Router 'auth' berhasil dimuat dari authApi.js"); 
-  router.use("/auth", auth);
-} else {
-  console.error("GAGAL memuat router 'auth' dari authApi.js!");
-}
-
-
+router.use("/auth", auth);
 router.use("/user", user);
 router.use("/chatbotApi", chatbotApi); 
 
