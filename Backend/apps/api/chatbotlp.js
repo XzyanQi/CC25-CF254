e50bb0cb286ec084a892fd15ba19f64714070ed7
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-02 22:03:12 +0700

    Update chatbotlp.js

--- Backend/apps/api/chatbotlp.js
+++ Backend/apps/api/chatbotlp.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const PYTHON_API_URL = "https://flask-production-61c8.up.railway.app/search";
 
 const sendToPythonService = async (text, top_kVal = 3) => {
-  // ... (isi fungsi seperti sebelumnya) ...
   try {
      console.log(`[Node.js Service - chatbotlp.js] Mengirim ke Python AI -> text: "${text}", top_k: ${top_kVal}`);
      const response = await axios.post(PYTHON_API_URL, { text, top_k: top_kVal });
