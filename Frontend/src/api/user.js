6a09b4db5351596114303affae1085c29288ca0e
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-02 22:11:27 +0700

    Update user.js

--- Frontend/src/api/user.js
+++ Frontend/src/api/user.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASH_URL = "http://localhost:3000"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 export const getUserByEmail = async (email) => {
     try {
