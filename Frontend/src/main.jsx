c5c7e1b2c7c9d1d6204cb0fc826a6cd4773fa5f3
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-04 23:09:25 +0700

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import ErrorBoundary from './ErrorBoundary';
 import App from './App.jsx'
 import './index.css'
-React
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
