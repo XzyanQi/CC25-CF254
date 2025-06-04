1e17200803307aa164cdc274adfa9daa8f51808a
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-05 04:13:22 +0700

    chatbotlp.js

--- Backend/apps/api/chatbotlp.js
+++ Backend/apps/api/chatbotlp.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
-
-const PYTHON_API_URL = process.env.PYTHON_API_URL || "http://localhost:8080";
+const PYTHON_API_URL = process.env.PYTHON_API_URL || "http://localhost:8080/search";
 
 const sendToPythonService = async (text, top_kVal = 3) => {
   try {
-     console.log(`[Node.js Service - chatbotlp.js] Mengirim ke Python AI -> text: "${text}", top_k: ${top_kVal}`);
-     const response = await axios.post(PYTHON_API_URL, { text, top_k: top_kVal });
-     console.log("[Node.js Service - chatbotlp.js] Menerima respons dari Python AI.");
-     return response.data;
-   } catch (error) {
-     console.error("[Node.js Service - chatbotlp.js] Gagal koneksi ke Python AI:", error.message);
-     throw new Error("Layanan AI Mindfulness tidak dapat dihubungi.");
-   }
+    const response = await axios.post(PYTHON_API_URL, { text, top_k: top_kVal });
+    return response.data;
+  } catch (error) {
+    throw new Error("Layanan AI Mindfulness tidak dapat dihubungi.");
+  }
 };
 
 module.exports = { sendToPythonService };
