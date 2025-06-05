d67b9c77d7cc3490e9066338ebe97fb68f3a7703
Faqih Muhammad Ihsan <134912000+XzyanQi@users.noreply.github.com>
2025-06-05 16:37:50 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const routes = require('./routes/route'); 
 
+const app = express();
 
+// cors
 app.use(cors({
-  origin: "https://mindfulnessnlp.vercel.app", 
-  credentials: true
+  origin: "https://mindfulnessnlp.vercel.app",
+  credentials: true,
+  // method
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.options('*', cors());
+
 app.use(express.json());
-
-
-app.use('/api', routes); 
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API root! Actual endpoints are likely under /api');
+  res.send('Welcome to the API root! Actual endpoints are likely under /api');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}, API base path /api`);
+  console.log(`Server is running on port ${PORT}, API base path /api`);
 });
